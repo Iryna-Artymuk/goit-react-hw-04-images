41311faca65775084ee65fac6eb0a8973da162d4
Iryna Artymuk <artimyk@ukr.net>
2023-03-23 22:04:03 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
+// import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
 import { AiOutlineClose } from 'react-icons/ai';
 import IconButton from '../IconButton/IconButton';
-const modalRoot = document.querySelector('#modal-root');
+// const modalRoot = document.querySelector('#modal-root');
 export default function Modal({ toggleModal, children }) {
   const closeOnBackdropClick = event => {
     if (event.target === event.currentTarget) {
